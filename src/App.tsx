a1c4f80f63f4f9365cce9983ffc368ff2e6c82b7
harshit Mishra <miananya2@gmail.com>
2024-06-17 05:43:48 +0530

    simple use of creteSore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { create } from "zustand";
 import { Button } from "./components/ui/button";
 
+// stores in zustand are simply hooks
+// zustand provides the set utility
+
+const useStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
+
+function Count() {
+  const store = useStore();
+  return <>{store.count}</>;
+}
+
 function App() {
+  const store = useStore();
   return (
     <>
-      <Button>Button</Button>
+      <Button onClick={store.inc}>+</Button>
+      <Count />
+      <Button onClick={store.dec}>-</Button>
     </>
   );
 }
