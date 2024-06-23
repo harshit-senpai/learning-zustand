a2013727c1fd305f34eb238e3e31a30acae99fb4
harshit Mishra <miananya2@gmail.com>
2024-06-23 13:30:06 +0530

    final store and cartSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { Store } from "@/types/store";
 import { create } from "zustand";
 import { createUserSlice } from "./userSlice";
+import { createCartSlice } from "./cartSlice";
 import { immer } from "zustand/middleware/immer";
 
 export const useStore = create<Store>()(
   immer((...a) => ({
     ...createUserSlice(...a),
+    ...createCartSlice(...a),
   }))
 );
--- src/types/store.ts
+++ src/types/store.ts
@@ -1,3 +1,4 @@
+import { CartSlice } from "@/store/cartSlice";
 import { UserSlice } from "@/store/userSlice";
 
-export type Store = UserSlice;
+export type Store = UserSlice & CartSlice;
